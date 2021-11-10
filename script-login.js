eab043cb756579b4638a9ffe0890bfdaca69d1bf
Enzo Sabino <contato.enzosabino@gmail.com>
2021-11-10 20:18:30 -0300

    Login HTML and JS

--- script-login.js
+++ script-login.js
@@ -1,18 +1,18 @@
-/**
- * verificar se o email possui um "@" e um ".com"
- */
-
 function validateForm(){
     let email = document.getElementById("email").value;
     let password = document.getElementById("password").value;
 
-    if (!email) {
+    if (!email || !(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email))) {
         document.getElementById("email").classList.add("input-err");
         document.getElementById("email-err").classList.remove("hidden");
     }
 
-    if (!password) {
+    if (!password){
         document.getElementById("password").classList.add("input-err");
         document.getElementById("password-err").classList.remove("hidden");
+    }
+
+    if (email && password){
+        window.location="./index-home.html";
     }
 }
