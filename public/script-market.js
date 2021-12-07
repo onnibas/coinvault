9b478af0e4904daf46d74af1f28021aae70e7d67
Enzo Sabino <contato.enzosabino@gmail.com>
2021-12-07 15:24:39 -0300

    Corrections on Market's search() function

--- public/script-market.js
+++ public/script-market.js
@@ -1,12 +1,21 @@
 function search(){
     let input = document.getElementById("search").value
-    console.info(input)
     
     document.getElementById("btc").classList.add("hidden")
     document.getElementById("eth").classList.add("hidden")
     document.getElementById("doge").classList.add("hidden")
     document.getElementById("xrp").classList.add("hidden")
     document.getElementById("ada").classList.add("hidden")
+    
+    if(!input){
+        document.getElementById("btc").classList.remove("hidden")
+        document.getElementById("eth").classList.remove("hidden")
+        document.getElementById("doge").classList.remove("hidden")
+        document.getElementById("xrp").classList.remove("hidden")
+        document.getElementById("ada").classList.remove("hidden")
+    }
+    else{
+        document.getElementById(input).classList.remove("hidden")
+    }
 
-    document.getElementById(input).classList.remove("hidden")
 }
